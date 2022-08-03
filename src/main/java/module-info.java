46bc17e9ac45bf4c985a83227059a693c85b0de0
James Seibel <jeseibel@gondtc.com>
2022-08-02 21:40:14 -0500

    Add (commented out) module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+//module dhApi
+//{
+//	requires org.apache.logging.log4j;
+//	requires org.lwjgl.opengl;
+//	requires java.datatransfer;
+//	requires com.google.common;
+//	requires java.desktop;
+//	requires org.lwjgl.glfw;
+//	requires com.formdev.flatlaf;
+//	requires json.simple; // not found
+//	requires core; // electronwill.nightconfig
+//	requires com.formdev.flatlaf.extras;
+//
+//	// annotations
+//	requires org.jetbrains.annotations; // not found
+//	requires jsr305; // not found
+//
+//
+//	// Distant Horizons' API exports
+//	exports com.seibel.lod.core.api.external;
+//}
