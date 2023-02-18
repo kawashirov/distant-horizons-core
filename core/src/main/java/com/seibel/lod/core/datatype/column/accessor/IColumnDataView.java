912a7687c31f43afd65d9b6ff4f2ff3a246add55
James Seibel <jeseibel@gondtc.com>
2023-02-18 11:25:09 -0600

    refactor columnArrayView and interface

--- core/src/main/java/com/seibel/lod/core/datatype/column/accessor/IColumnDataView.java
+++ core/src/main/java/com/seibel/lod/core/datatype/column/accessor/IColumnDataView.java
@@ -2,32 +2,39 @@ package com.seibel.lod.core.datatype.column.accessor;
 
 import java.util.Iterator;
 
-public interface IColumnDataView {
-    long get(int index);
-
-    int size();
-
-    default Iterator<Long> iterator() {
-        return new Iterator<Long>() {
-            private int index = 0;
-            private final int size = size();
-            @Override
-            public boolean hasNext() {
-                return index < size;
-            }
-
-            @Override
-            public Long next() {
-                return get(index++);
-            }
-        };
-    }
-
-    int verticalSize();
-
-    int dataCount();
-
-    IColumnDataView subView(int dataIndexStart, int dataCount);
-
-    void copyTo(long[] target, int offset, int count);
+public interface IColumnDataView
+{
+	long get(int index);
+	
+	int size();
+	
+	default Iterator<Long> iterator()
+	{
+		return new Iterator<Long>()
+		{
+			private int index = 0;
+			private final int size = size();
+			
+			@Override
+			public boolean hasNext()
+			{
+				return index < size;
+			}
+			
+			@Override
+			public Long next()
+			{
+				return get(index++);
+			}
+		};
+	}
+	
+	int verticalSize();
+	
+	int dataCount();
+	
+	IColumnDataView subView(int dataIndexStart, int dataCount);
+	
+	void copyTo(long[] target, int offset, int count);
+	
 }
